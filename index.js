888c381ff03f9cd15418b6f9e9844575a1aa3d4f
Cameron Dodd <cameron.dodd@mavs.uta.edu>
2017-09-28 14:56:40 -0400

    added feature to convert state abbreviations into full words to feed into getFood, changed the city search button from Here to Submit, adjusted padding and margins to several things, and vertically centered h2

--- index.js
+++ index.js
@@ -119,6 +119,109 @@ const foodURL="https://developers.zomato.com/api/v2.1/search";
 
 const eventURL="https://api.eventful.com/jsonp/events/search";
 
+const stateList=[
+    	{abr:'AL',
+    	full:'Alabama'},
+    	{abr:'AK',
+    	full:'Alaska'},
+    	{abr:'AZ',
+    	full:'Arizona'},
+    	{abr:'AR',
+    	full:'Arkansas'},
+    	{abr:'CA',
+    	full:'California'},
+    	{abr:'CO',
+    	full:'Colorado'},
+    	{abr:'CT',
+    	full:'Connecticut'},
+    	{abr:'DE',
+    	full:'Delaware'},
+    	{abr:'FL',
+    	full:'Florida'},
+    	{abr:'GA',
+    	full:'Georgia'},
+    	{abr:'HI',
+    	full:'Hawaii'},
+    	{abr:'ID',
+    	full:'Idaho'},
+    	{abr:'IL',
+    	full:'Illinois'},
+    	{abr:'IN',
+    	full:'Indiana'},
+    	{abr:'IA',
+    	full:'Iowa'},
+    	{abr:'KS',
+    	full:'Kansas'},
+    	{abr:'KY',
+    	full:'Kentucky'},
+    	{abr:'LA',
+    	full:'Louisiana'},
+    	{abr:'ME',
+    	full:'Maine'},
+    	{abr:'MD',
+    	full:'Maryland'},
+    	{abr:'MA',
+    	full:'Massachusetts'},
+    	{abr:'MI',
+    	full:'Michigan'},
+    	{abr:'MN',
+    	full:'Minnesota'},
+    	{abr:'MS',
+    	full:'Mississippi'},
+    	{abr:'MO',
+    	full:'Missouri'},
+    	{abr:'MT',
+    	full:'Montana'},
+    	{abr:'NE',
+    	full:'Nebraska'},
+    	{abr:'NV',
+    	full:'Nevada'},
+    	{abr:'NH',
+    	full:'New Hampshire'},
+    	{abr:'NJ',
+    	full:'New Jersey'},
+    	{abr:'NM',
+    	full:'New Mexico'},
+    	{abr:'NY',
+    	full:'New York'},
+    	{abr:'NC',
+    	full:'North Carolina'},
+    	{abr:'ND',
+    	full:'North Dakota'},
+    	{abr:'OH',
+    	full:'Ohio'},
+    	{abr:'OK',
+    	full:'Oklahoma'},
+    	{abr:'OR',
+    	full:'Oregon'},
+    	{abr:'PA',
+    	full:'Pennsylvania'},
+    	{abr:'RI',
+    	full:'Rhode Island'},
+    	{abr:'SC',
+    	full:'South Carolina'},
+    	{abr:'SD',
+    	full:'South Dakota'},
+    	{abr:'TN',
+    	full:'Tennessee'},
+    	{abr:'TX',
+    	full:'Texas'},
+    	{abr:'UT',
+    	full:'Utah'},
+    	{abr:'VT',
+    	full:'Vermont'},
+    	{abr:'VA',
+    	full:'Virginia'},
+    	{abr:'WA',
+    	full:'Washington'},
+    	{abr:'WV',
+    	full:'West Virginia'},
+    	{abr:'WI',
+    	full:'Wisconsin'},
+    	{abr:'WY',
+    	full:'Wyoming'}
+    ];
+
 //CITY INFORMATION FOR EVENT AND RESTAURANT FEATURES
 
 function citySubmit(){
@@ -127,15 +230,52 @@ function citySubmit(){
     const queryTarget=$(event.currentTarget).find('.cityInput');
     const cityInput=queryTarget.val();
     queryTarget.val('');
-    console.log('citySubmit ran with search term '+cityInput);
-    getCity(cityInput,entityFind);
-    eventCity=cityInput;
-    $('.foodRequestContainer').prop('hidden',false);
-    $('.eventRequestContainer').prop('hidden',false);
-    $('.cityResults').prop('hidden',false);
-    $('.cityResults').html('');
-    $('.cityResults').html(`I hear good things about ${cityInput}!`);
-  });
+    const splitCityInput=cityInput.split(',');
+    const city=splitCityInput[0];
+    const stateFat=splitCityInput[1];
+    const state=stateFat.trim();
+    
+    if (state.length>2){
+	    console.log('citySubmit ran with search term '+cityInput);
+	    getCity(cityInput,entityFind);
+	    eventCity=cityInput;
+	    $('.foodRequestContainer').prop('hidden',false);
+	    $('.eventRequestContainer').prop('hidden',false);
+	    $('.cityResults').prop('hidden',false);
+	    $('.cityResults').html('');
+	    $('.cityResults').html(`I hear good things about ${cityInput}!`);
+		}
+	  
+	else if (state.length===2){
+	  	newStateFind(state);
+		let newCityInput=`${city}, ${newState}`;
+		console.log('citySubmit ran with search term '+newCityInput);
+	    getCity(newCityInput,entityFind);
+	    eventCity=cityInput;
+	    $('.foodRequestContainer').prop('hidden',false);
+	    $('.eventRequestContainer').prop('hidden',false);
+	    $('.cityResults').prop('hidden',false);
+	    $('.cityResults').html('');
+	    $('.cityResults').html(`I hear good things about ${newCityInput}!`);
+	}
+	
+	else {
+		$('.cityResults').prop('hidden',false);
+		$('.cityResults').html('');
+		$('.cityResults').html(`I'm not sure ${state} is a state.`);
+		}
+	});
+}
+
+function newStateFind(state){
+  	console.log('newStateFind ran');
+	for (let i=0;i<stateList.length;i++){
+	    if (stateList[i].abr===state){
+	        newState=stateList[i].full;
+	        console.log(newState);
+	    	return newState;
+	  	}
+	}
 }
 
 function getCity(searchTerm,callback){
